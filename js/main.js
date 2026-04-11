81049c3f3a7040dfa2819f7a30857a3a5e2e2410
Kacper <loliks.elix@gmail.com>
2026-04-11 18:40:15 +0200

    add hero-img section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector('.nav')
 const navButton = document.querySelector('.burger-button')
 const navLinks = document.querySelectorAll('.nav__link')
+const section = document.querySelectorAll('.section')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
