6271f5a54a23a6c89eed1a7ff8c5c43f8734f588
Kacper <loliks.elix@gmail.com>
2026-04-16 21:06:00 +0200

    add footer

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const nav = document.querySelector('.nav')
 const navButton = document.querySelector('.burger-button')
 const navLinks = document.querySelectorAll('.nav__link')
 const section = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--active')
@@ -11,5 +12,12 @@ const handleNavLinks = () => {
 	nav.classList.remove('nav--active')
 }
 
+const handleFooterYear = () => {
+	const now = new Date()
+	footerYear.textContent = `® ${now.getFullYear()} Tropical Paradise`
+}
+
 navButton.addEventListener('click', handleNav)
 navLinks.forEach(item => item.addEventListener('click', handleNavLinks))
+
+handleFooterYear()
